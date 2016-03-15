7b540c7d713c130c483fd56756af4901ec6475dd
Andrei Scripcaru <andrei.scripcaru@gmail.com>
2016-03-15 22:44:16 +0200

    socket.io improvement

--- routes/mqtt.js
+++ routes/mqtt.js
@@ -2,28 +2,9 @@ var mqtt    = require('mqtt');
 var client  = mqtt.connect([{ host: 'localhost', port: 1883 }]);
 var express = require('express');
 var router = express.Router();
-var io  = require('socket.io').listen(5000);
 var sensorList = require('../data/sensors.json');
 
-io.sockets.on('connection', function (socket) {
-	  socket.on('subscribe', function (data) {
-	    console.log('Subscribing to ' + data.topic);
-	    client.subscribe(data.topic);
-	  });
-	});
-
-
-client.on('connect', function () {
-// client.subscribe('test');
-// client.publish('test', 'Hello mqtt');
-});
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-
-  io.sockets.emit('mqtt',{'topic':String(topic), 'payload': message.toString()});  
-});
+var socketio = require('socket.io').listen(5000);
 
 router.get('/', function(req, res, next) {
 	  res.render('mqtt', {	sensors: sensorList, 
@@ -31,3 +12,28 @@ router.get('/', function(req, res, next) {
 	});
 
 module.exports = router;
+
+module.exports.socketServer = function (app, server){
+	io = socketio.listen(server);
+	
+	io.sockets.on('connection', function (socket) {
+		  socket.on('subscribe', function (data) {
+		    console.log('Subscribing to ' + data.topic);
+		    client.subscribe(data.topic);
+		  });
+		});
+	
+	
+	client.on('connect', function () {
+	// client.subscribe('test');
+	// client.publish('test', 'Hello mqtt');
+	});
+	 
+	client.on('message', function (topic, message) {
+	  // message is Buffer
+	  console.log(message.toString());
+	
+	  io.sockets.emit('mqtt',{'topic':String(topic), 'payload': message.toString()});  
+	});
+};
+
